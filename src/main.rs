137c0ec0bb9b7ffd7a2b6632c0b73a9edafb66c1
David Pederson <dpederson@figure.com>
2024-03-19 19:51:31 -0600

    create story

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 use atoi::atoi;
 use matchit::{Match, Params, Router};
 use smallvec::SmallVec;
-use std::io;
+use std::io::{self, Read};
 use std::sync::Arc;
 use yarte::Serialize;
 
@@ -16,6 +16,7 @@ use may_postgres::{self, Client, Statement};
 // SQL queries
 const SQL_SELECT_STORIES: &str = "select * from stories limit 32";
 const SQL_SELECT_STORY: &str = "select * from stories where id = $1";
+const SQL_INSERT_STORY: &str = "insert into stories (name) values ($1) returning id";
 
 // Error codes
 const ERROR_SQL_QUERY: u8 = 1;
@@ -38,6 +39,7 @@ pub struct Story {
 struct PgStatements {
     select_stories: Statement,
     select_story: Statement,
+    insert_story: Statement,
 }
 
 struct PgConnection {
@@ -51,10 +53,12 @@ impl PgConnection {
 
         let select_stories = client.prepare(SQL_SELECT_STORIES).unwrap();
         let select_story = client.prepare(SQL_SELECT_STORY).unwrap();
+        let insert_story = client.prepare(SQL_INSERT_STORY).unwrap();
 
         let statements = Arc::new(PgStatements {
             select_stories,
             select_story,
+            insert_story,
         });
 
         Self { client, statements }
@@ -85,12 +89,33 @@ impl PgConnection {
             .query_raw(&self.statements.select_story, &[&id])
             .map_err(|_| ERROR_SQL_QUERY)?;
 
-        match q.next().transpose().map_err(|_| ERROR_SQL_QUERY)? {
-            Some(row) => Ok(Story {
+        if let Some(result) = q.next() {
+            let row = result.map_err(|_| ERROR_SQL_QUERY)?;
+            let story = Story {
                 id: row.get(0),
                 name: row.get(1),
-            }),
-            None => Err(ERROR_NOT_FOUND),
+            };
+            Ok(story)
+        } else {
+            Err(ERROR_NOT_FOUND)
+        }
+    }
+
+    fn create_story(&self, name: &str) -> Result<Story, u8> {
+        let mut q = self
+            .client
+            .query_raw(&self.statements.insert_story, &[&name])
+            .map_err(|_| ERROR_SQL_QUERY)?;
+
+        if let Some(result) = q.next() {
+            let row = result.map_err(|_| ERROR_SQL_QUERY)?;
+            let id: i32 = row.get(0);
+            Ok(Story {
+                id,
+                name: name.into(),
+            })
+        } else {
+            Err(ERROR_NOT_FOUND)
         }
     }
 }
@@ -126,15 +151,33 @@ struct TodoService {
 }
 
 impl TodoService {
+    fn new(db: PgConnection) -> Self {
+        let mut router = Router::new();
+        router.insert("/stories", STORIES).unwrap();
+        router.insert("/stories/{id}", STORY).unwrap();
+        Self { router, db }
+    }
+
+    #[inline(always)]
+    fn dispatch(&self, route: Match<'_, '_, &u8>, method: &str, body: &[u8], rsp: &mut Response) {
+        match (method, route.value) {
+            ("GET", &STORIES) => self.get_stories(rsp),
+            ("GET", &STORY) => self.get_story(route.params, rsp),
+            ("POST", &STORIES) => self.create_story(body, rsp),
+            _ => {
+                rsp.status_code(404, "");
+                ()
+            }
+        }
+    }
+
     #[inline(always)]
     fn get_stories(&self, rsp: &mut Response) {
         let stories = self.db.get_stories().unwrap_or_default();
         rsp.header("Content-Type: application/json");
         stories.to_bytes_mut(rsp.body_mut());
     }
-}
 
-impl TodoService {
     #[inline(always)]
     fn get_story(&self, params: Params, rsp: &mut Response) {
         let param = params.get("id").unwrap_or_default();
@@ -150,29 +193,58 @@ impl TodoService {
             rsp.status_code(404, "");
         }
     }
-}
 
-impl TodoService {
     #[inline(always)]
-    fn dispatch(&self, route: Match<'_, '_, &u8>, req: &Request, rsp: &mut Response) {
-        match (req.method(), route.value) {
-            ("GET", &STORIES) => self.get_stories(rsp),
-            ("GET", &STORY) => self.get_story(route.params, rsp),
-            _ => {
-                rsp.status_code(404, "");
-                ()
-            }
+    fn create_story(&self, body: &[u8], rsp: &mut Response) {
+        let value: serde_json::Value = serde_json::from_slice(body).unwrap_or_default();
+        if !value.is_object() {
+            rsp.status_code(400, "");
+            return;
+        }
+
+        let name = value
+            .as_object()
+            .map(|o| o.get("name"))
+            .flatten()
+            .map(|o| o.as_str())
+            .flatten()
+            .unwrap_or_default();
+
+        if name.is_empty() {
+            rsp.status_code(400, "");
+            return;
+        }
+
+        if let Ok(story) = self.db.create_story(&name) {
+            rsp.status_code(201, "");
+            rsp.header("Content-Type: application/json");
+            story.to_bytes_mut(rsp.body_mut());
+        } else {
+            rsp.status_code(400, "");
         }
     }
 }
 
 impl HttpService for TodoService {
     fn call(&mut self, req: Request, rsp: &mut Response) -> io::Result<()> {
-        if let Ok(route) = self.router.at(req.path()) {
-            self.dispatch(route, &req, rsp);
+        let path = req.path().to_owned();
+        let method = req.method().to_owned();
+        let mut body = req.body();
+
+        let mut buf = String::new();
+        if method == "POST" {
+            if let Err(_) = body.read_to_string(&mut buf) {
+                rsp.status_code(400, "");
+                return Ok(());
+            }
+        }
+
+        if let Ok(route) = self.router.at(&path) {
+            self.dispatch(route, &method, buf.as_bytes(), rsp);
         } else {
             rsp.status_code(404, "");
         }
+
         Ok(())
     }
 }
@@ -185,12 +257,10 @@ struct HttpServer {
 
 impl HttpServiceFactory for HttpServer {
     type Service = TodoService;
+
     fn new_service(&self, id: usize) -> Self::Service {
         let db = self.pool.get_connection(id);
-        let mut router = Router::new();
-        router.insert("/stories", STORIES).unwrap();
-        router.insert("/stories/{id}", STORY).unwrap();
-        TodoService { router, db }
+        TodoService::new(db)
     }
 }
 
